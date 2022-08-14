80476e4c0bca7a53f2e6f9fd13e1974cd2fa4d34
dfpuentes <diego.puentes@hogaru.com>
2022-08-14 06:33:22 +0000

    att accesor image

--- app/controllers/translates_controller.rb
+++ app/controllers/translates_controller.rb
@@ -1,13 +1,13 @@
 class TranslatesController < ActionController::API
-    attr_accessor :base
+    attr_accessor :image
     def index
         render :json => {:success=>true, :message=>"Request ok"}, status: 200
     end
 
     def create
-        str = Base64.encode64(File.read("app/images/62.png"))
+        #str = Base64.encode64(File.read("app/images/62.png"))
         
-        s = Base64.decode64(str)
+        s = Base64.decode64(params[:image])
 
         file_name = rand(1000)
         path = "tmp/#{rand}.png"

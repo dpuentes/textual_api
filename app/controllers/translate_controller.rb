95df0527347d94ad3f10aabd41193db835f4d1ef
dfpuentes <diego.puentes@hogaru.com>
2022-08-14 01:53:54 +0000

    beta version

---
+++ app/controllers/translate_controller.rb
@@ -0,0 +1,19 @@
+class TranslateController < ActionController::API
+    def create
+        str = Base64.encode64(File.read("app/images/62.png"))
+        
+        s = Base64.decode64(str)
+
+        file_name = rand(1000)
+        path = "app/images/#{rand}.png"
+
+        File.open(path, 'wb') do |f|
+            f.write(s)
+        end
+        image = RTesseract.new(path)
+        puts image.to_s
+
+
+        render :json => {:success=>true, :message=>"Request ok"}, status: 200
+    end
+end
